307940409bf068502d5f76057759d332dd46c15c
Dhatchayini <Dhatchayini@LAPTOP-L6QEV7TQ>
2021-03-22 18:14:40 +0530

    change

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -5,10 +5,11 @@ public class Calculator {
 	public static void main(String[] args) {
       System.out.println("Hello World");
       
-      public static void add(int num1,int num2){
-          System.out.println(num1+num2);
-      }
-      
+     
 	}
+	 public static void add(int num1,int num2){
+         System.out.println(num1+num2);
+     }
+     
 
 }
