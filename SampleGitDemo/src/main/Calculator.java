89f1592517bb5f0c9f6c71628c43e36a91f0cb37
Dhatchayini <Dhatchayini@LAPTOP-L6QEV7TQ>
2021-03-23 10:53:47 +0530

    added br1 func 1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -27,9 +27,6 @@ public class Calculator {
          System.out.println(num1%num2);
      }
 
-	
-	 
-
 	public static void sqaure(int num1){
 		System.out.println(num1*num1);
 	}
@@ -37,5 +34,7 @@ public class Calculator {
 	public static void four(int num1){
 		System.out.println(num1*num1*num1*num1);
 	}
-    
+	public static void br1func1(int num1){
+		System.out.println(num1*num1*num1*num1);
+	}
 }
