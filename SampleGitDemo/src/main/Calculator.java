292f027720d0f2a3858fc92b9ca9a866bcc2a7fe
Dhatchayini <Dhatchayini@LAPTOP-L6QEV7TQ>
2021-03-23 10:18:23 +0530

    added cube

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
       System.out.println("Hello World");
       add(10,20);
       
+      
      
 	}
 	 public static void add(int num1,int num2){
@@ -25,4 +26,8 @@ public class Calculator {
 	 public static void mod(int num1,int num2){
          System.out.println(num1%num2);
      }
+	 public static void cube(int num1){
+         System.out.println(num1*num1*num1);
+     }
+	 
 }
